fe7ce557821a4d0446b7c36ab780ce3865e7654d
SongWonHea <ww_song@naver.com>
2020-03-12 07:07:26 +0900

    code menu and project menu

--- react_src/intranet/views/Project/Manage/Manage.js
+++ react_src/intranet/views/Project/Manage/Manage.js
@@ -1,15 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import ProjectGraph from './component/ProjectGraph';
+import ProjectContents from './component/ProjectContents';
+import InputSearch from './component/InputSearch';
+import SelectType from './component/SelectType';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+
 
 class Manage extends Component {
 	render() {
 		return (
-			<Card>
-				<CardContent>
-					프로젝트관리 
-				</CardContent>
-			</Card>
+			<Fragment>
+				<Card>
+					<CardContent>
+						프로젝트관리
+						<ProjectGraph/>
+					</CardContent>
+				</Card>
+				<br/>
+				<Card>
+					<CardContent>
+						프로젝트관리 
+						<Toolbar>
+							<Button variant="contained" color="primary">
+								프로젝트 등록
+							</Button>
+							<SelectType/>
+                    		<InputSearch/>
+						</Toolbar>
+						<ProjectContents/>
+					</CardContent>
+				</Card>
+			</Fragment>
 		);
 	}
 }
