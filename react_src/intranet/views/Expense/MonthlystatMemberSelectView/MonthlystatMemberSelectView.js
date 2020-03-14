08e65adb4e789e1c250bb78c4a1319a45d93de42
dbrlg <dbrlg@DESKTOP-63EE672>
2020-03-14 18:44:01 +0900

    월별통계

--- react_src/intranet/views/Expense/MonthlystatMemberSelectView/MonthlystatMemberSelectView.js
+++ react_src/intranet/views/Expense/MonthlystatMemberSelectView/MonthlystatMemberSelectView.js
@@ -6,29 +6,55 @@
  */
 
 import React, { Fragment } from 'react';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import Table from '@material-ui/core/Table';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
+import moment from 'moment';
+
 import Fab from '@material-ui/core/Fab';
 import SaveIcon from '@material-ui/icons/Save';
 import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
 import {useStyles} from './styles';
-import {dataAdmin2020, dataAdmin2019, dataAdmin2018, dataAdmin2017, nodata} from './data';
+import {data_2020_01, data_2019_12, data_2019_11
+	, data_2019_10, data_2019_09, data_2019_08
+	, data_2019_07, nondata} from './data';
 
 export default function MonthlystatMemberSelectView() {
-	const classes = useStyles();	// styles.js에 상수로 선언되어 있음. 
+	const classes = useStyles();				// styles.js에 상수로 선언되어 있음. 
+	
 	const [state, setState] = React.useState({	// state 생성 (hooks 구조임)
-		year: '', 
-		members : dataAdmin2020.members
+		members : data_2020_01.members,
+		year : "",
+		month : "",
 	});
-	const { members } = state;
-	const headerCells = [
-		"성명","구분 ", "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월", "합계", "총합계"
-	];
+	
+	const members	= state.members;
+	const monthArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; 
+
+	const [open, setOpen] = React.useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const Transition = React.forwardRef(function Transition(props, ref) {
+		return <Slide direction="up" ref={ref} {...props} />;
+	});
 
 	// Select로 구성할 년도 목록
 	const getYearList = (period) => {
@@ -37,126 +63,197 @@ export default function MonthlystatMemberSelectView() {
 		return yearArr.fill(0).map( (value, idx) => {
 			return d.getFullYear() - idx;
 		});
-	}
-	
+	};
+
 	const excelDownload = (e) => {
 		alert('엑셀다운로드 실행');
-	}
-
-
-	const inputLabel = React.useRef(null);
-	const [labelWidth, setLabelWidth] = React.useState(0);
-	React.useEffect(() => {
-		setLabelWidth(inputLabel.current.offsetWidth);
-	}, []);
-
+	};
 	/*
 	 * 년도 변경 시, 호출
 	 * data.js에 더미 데이터 생성하여 로드
 	*/
-	const handleChange = name => event => {
+	const YearSelectBoxChange = name => event => {
+
 		let memberData = {};
 		console.log(event.target.value);
-		if(event.target.value == 2019) {
-			memberData = dataAdmin2019.members;
-		} else if(event.target.value == 2018) {
-			memberData = dataAdmin2018.members;
-		} else if(event.target.value == 2017) {
-			memberData = dataAdmin2017.members;
-		} else {
-			memberData = dataAdmin2020.members;
-		}
+
 		
+
+		if(event.target.value == 2020) {	
+			switch(month){
+				case 1 :
+					memberData = data_2020_01.members;
+					break;
+				default : 
+					memberData = nondata.members;
+					break;
+			}
+		} else if(event.target.value == 2019) {
+			switch(month){
+				case 7 :
+					memberData = data_2019_07.members;
+					break;
+				case 8 : 
+					memberData = data_2019_08.members;
+					break;
+				case 9 : 
+					memberData = data_2019_09.members;
+					break;
+				case 10 : 
+					memberData = data_2019_10.members;
+					break;
+				case 11 : 
+					memberData = data_2019_11.members;
+					break;
+				case 12 : 
+					memberData = data_2019_12.members;
+					break;
+				default : 
+					memberData = nondata.members;
+					break;
+			}
+		} else {
+			memberData = nondata.members;
+		}
+
 		if(memberData === undefined || memberData.length ==0) {
 			memberData = nodata;
 		}
 		
+		setState({
+			...state,
+			[name]: event.target.value,
+			year : event.target.value,
+			members: memberData,
+		});
+	};
+
+	/*
+	 * 달 변경 시, 호출
+	 * data.js에 더미 데이터 생성하여 로드
+	*/
+
+	const MonthSelectBoxChange = name => event => {3
+
+		let memberData = {};
+		console.log(event.target.value);
+
+		month = event.target.value;
+
+		if(year == 2020) {	
+			switch(event.target.value){
+				case 1 :
+					memberData = data_2020_01.members;
+					break;
+				default : 
+					memberData = nondata.members;
+					break;
+			}
+		} else if(year == 2019) {
+			switch(event.target.value){
+				case 7 :
+					memberData = data_2019_07.members;
+					break;
+				case 8 : 
+					memberData = data_2019_08.members;
+					break;
+				case 9 : 
+					memberData = data_2019_09.members;
+					break;
+				case 10 : 
+					memberData = data_2019_10.members;
+					break;
+				case 11 : 
+					memberData = data_2019_11.members;
+					break;
+				case 12 : 
+					memberData = data_2019_12.members;
+					break;
+				default : 
+					memberData = nondata.members;
+					break;
+			}
+		} else {
+			memberData = nondata.members;
+		}
+
+		if(memberData === undefined || memberData.length ==0) {
+			memberData = nodata;
+		}
 		
 		setState({
 			...state,
 			[name]: event.target.value,
+			month : event.target.value,
 			members: memberData,
 		});
 	};
 	
 	return (
 		<Fragment>
-			<FormControl variant="outlined" className={classes.formControl} >
-				<InputLabel ref={inputLabel} htmlFor="outlined-age-native-simple">
-					Year
-				</InputLabel>
-				<Select
-					native
-					value={state.buttonYear}
-					onChange={handleChange('buttonYear')}
-					labelWidth={labelWidth}
-					inputProps={{
-						name: 'buttonYear',
-						id: 'outlined-age-native-simple',
-					}}
-					className={classes.select}
-				>
-					{getYearList(4).map((value, idx) => (
-						<option key={idx} value={value}>{value}</option>
-					))};
-				</Select>
-			</FormControl>
+			<Card>
+				<CardContent>
+					<FormControl variant="outlined" className={classes.formControl} >
+						<NativeSelect
+							value={state.buttonYear}
+							onChange={MonthSelectBoxChange('buttonYear')}
+							inputProps={{
+								name: 'buttonYear',
+								id: 'outlined-age-native-simple',
+							}}
+							className={classes.selectEmpty}
+							>
+							{getYearList(5).map((value, idx) => (
+								<option key={idx} value={value}>{value}</option>
+							))};
+						</NativeSelect>
+					</FormControl>
+					<FormControl className={classes.formControl}>
+						<NativeSelect
+							value={state.buttonMonth}
+							onChange={MonthSelectBoxChange('buttonMonth')}
+							inputProps={{
+								name: 'buttonMonth',
+								id: 'outlined-age-native-simple',
+							}}
+							className={classes.selectEmpty}
+							>
+							{monthArr.map((value, idx) => (
+								<option key={idx} value={value}>{value}</option>
+							))};
+						</NativeSelect>
+					</FormControl>
+					<div style={{height : 20}}/>
+					<Grid container spacing={3}>
+						{members.map((value, idx) => (
+							<Grid item xs={12} key={idx}>
+								<Paper className={classes.paper} onClick={handleClickOpen}>{`${value.name} / ${value.position} / ${value.totalExpense} 원`} </Paper>
+							</Grid>
+						))}
+						{/* <option key={idx} value={value}>{value}</option> */}
+						
+					</Grid>
+				</CardContent>
+			</Card>
+			<Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
+				<AppBar className={classes.appBar}>
+				<Toolbar>
+					<IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+					<CloseIcon />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+					Sound
+					</Typography>
+					<Button autoFocus color="inherit" onClick={handleClose}>
+					save
+					</Button>
+				</Toolbar>
+				</AppBar>
+			</Dialog>
 			<Fab variant="extended" color="primary" aria-label="add" className={classes.fab} onClick={excelDownload}>
 				<SaveIcon className={classes.saveIcon} />
 				엑셀다운로드
 			</Fab>
-
-			<Table>
-				<TableHead className={ classes.tableHead }>								
-					<TableRow>
-						{headerCells.map((cell, idx) => (
-							<TableCell key={idx} align={'center'} className={classes.tableHeadCell}>
-								{cell}
-							</TableCell>
-						))}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-				{members.map((member, idx) => (
-					<Fragment key={`row${idx}`}>
-						<TableRow>
-							{/* 성명 */}
-							<TableCell align={'right'} rowSpan={2} className={classes.tableBodyCell}>
-								{member.name}
-							</TableCell>
-							<TableCell  align={'center'} className={classes.tableBodyCell} style={{minWidth:'80px'}}>교통비</TableCell>
-							{/* 1월~12월 교통비*/}
-							{member.tptCosts.map((tptCost, idx) => (	
-								<TableCell key={idx} align={'right'} className={classes.tableBodyCell}>
-									{typeof tptCost === 'number' ? tptCost.toLocaleString() : tptCost}
-								</TableCell>
-							))}
-							{/* 교통비 합계*/}
-							<TableCell align={'right'} className={classes.tableBodyCell}>
-								{typeof member.tptCostsSum === 'number' ? member.tptCostsSum.toLocaleString() : member.tptCostsSum}
-							</TableCell>
-							{/* 교통비/통신비 총합계*/}
-							<TableCell align={'right'} rowSpan={2} className={classes.tableBodyCell}>
-								{typeof member.totalSum === 'number' ? member.totalSum.toLocaleString() : member.totalSum}
-							</TableCell>
-						</TableRow>
-						<TableRow>
-							<TableCell align={'center'} className={classes.tableBodyCell} style={{minWidth:'80px'}}>통신비</TableCell>
-							{/* 1월~12월 통신비*/}
-							{new Array(12).fill(0).map((foo, idx) => (
-								<TableCell key={idx} align={'right'} className={classes.tableBodyCell}>
-									{typeof member.cmncCost === 'number' ? member.cmncCost.toLocaleString() : member.cmncCost}
-								</TableCell>
-							))}
-							{/* 통신비 합계*/}
-							<TableCell align={'right'} className={classes.tableBodyCell}>
-								{typeof member.cmncCostSum === 'number' ? member.cmncCostSum.toLocaleString() : member.cmncCostSum}
-							</TableCell>
-						</TableRow>	
-					</Fragment>							
-				))}
-				</TableBody>
-			</Table>				
 		</Fragment>
 	);
 }
--- react_src/intranet/views/Expense/MonthlystatMemberSelectView/data.js
+++ react_src/intranet/views/Expense/MonthlystatMemberSelectView/data.js
@@ -1,116 +1,132 @@
-export const dataAdmin2020 = {
+
+
+export const data_2020_01 = {
 	members : [
 		{
-			name : "최문걸",
-			tptCosts : [0, 0, 0, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost : 50000,		// 통신비(Communication)
-			tptCostsSum: 90000,	// 교통비 합계
-			cmncCostSum: 600000,	// 통신비 합계
-			totalSum: 690000			// 교통비 + 통신비 총합
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
 		},
 		{
-			name: "강성우",
-			tptCosts: [0, 0, 0, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 40000,		// 통신비(Communication)
-			tptCostsSum: 90000,
-			cmncCostSum: 480000,
-			totalSum: 570000
-
-		}, 
-	]
-}
-
-export const dataAdmin2019 = {
-	members : [
-		{
-			name : "최문걸",
-			tptCosts : [10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost : 10000,		// 통신비(Communication)
-			tptCostsSum: 120000,	// 교통비 합계
-			cmncCostSum: 120000,	// 통신비 합계
-			totalSum: 240000			// 교통비 + 통신비 총합
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
 		},
 		{
-			name: "강성우",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
 
-		}, 
-		{
-			name: "김준선",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
-
-		}, 
-		{
-			name: "유기환",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
-
-		}, 
-		{
-			name: "송원회",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
-
-		}, 
 	]
 }
 
-export const dataAdmin2018 = {
+export const data_2019_12 = {
 	members : [
 		{
-			name: "김준선",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
-
-		}, 
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
+		},
 		{
-			name: "유기환",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
-
-		}, 
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
 		{
-			name: "송원회",
-			tptCosts: [0, 0, 0, 0, 0, 10000, 10000, 0, 10000, 10000, 10000, 10000],	// 교통비(Transportation)
-			cmncCost: 20000,		// 통신비(Communication)
-			tptCostsSum: 60000,
-			cmncCostSum: 240000,
-			totalSum: 300000
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
 
-		}, 
+	]
+}
+export const data_2019_11 = {
+	members : [
+		{
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
+		},
+		{
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+		{
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+
 	]
 }
 
-export const dataAdmin2017 = {
+export const data_2019_10 = {
+	members : [
+		{
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
+		},
+		{
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+		{
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+
+	]
 }
 
-export const nodata = [
-	{
-		name: "데이터가 없습니다.",
-		tptCosts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],	// 교통비(Transportation)
-		cmncCost: 0,		// 통신비(Communication)
-		tptCostsSum: 0,
-		cmncCostSum: 0,
-		totalSum: 0
-	}
-]
+export const data_2019_09 = {
+	members : [
+		{
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
+		},
+		{
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+		{
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+
+	]
+}
+
+export const data_2019_08 = {
+	members : [
+		{
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "50000"		// 월 경비 합계
+		},
+		{
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+		{
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "12000"		// 월 경비 합계
+		},
+
+	]
+}
+
+export const data_2019_07 = {
+}
+
+export const nondata = {
+}
