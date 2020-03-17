c1bb5a98e500d3464cc5b0f455581e908e6a0247
dbrlg <dbrlg@DESKTOP-56GMHGL>
2020-03-17 19:58:42 +0900

    경비월별통계 완료

--- react_src/intranet/views/Expense/MonthlystatMemberSelectView/data.js
+++ react_src/intranet/views/Expense/MonthlystatMemberSelectView/data.js
@@ -1,42 +1,290 @@
+export const data_2020_03 = {
+	members : [
+		{	
+			empno : "10001",
+			name : "강성우",				// 이름
+			position : "대리",			// 직책
+			totalExpense : "48000"		// 월 경비 합계
+		},
+		{
+			empno : "10003",
+			name : "김준선",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "30000"		// 월 경비 합계
+		},
+		{
+			empno : "10002",
+			name : "유기환",				// 이름
+			position : "사원",			// 직책
+			totalExpense : "24000"		// 월 경비 합계
+		},
+	],
+	expenseInfo : [
+		{
+			empno : "10001",			// 사번
+			no : "11",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200322",		// 결제일
+		},
+		{
+			empno : "10001",			// 사번
+			no : "10",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200320",		// 결제일
+		},
+		{
+			empno : "10001",			// 사번
+			no : "9",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200315",		// 결제일
+		},
+		{
+			empno : "10001",			// 사번
+			no : "7",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200322",		// 결제일
+		},
+		{
+			empno : "10002",			// 사번
+			no : "6",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20200322",		// 결제일
+		},
+		{
+			empno : "10002",			// 사번
+			no : "5",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20200312",		// 결제일
+		},
+		{
+			empno : "10002",			// 사번
+			no : "4",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20200311",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "3",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20200312",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "2",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20200305",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "1",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20200302",		// 결제일
+		},
+	],
+	totalExpense : "102000",
+}
 
+export const data_2020_02 = {
+	members : [],
+	expenseInfo : [],
+	totalExpense : "0",
+}
 
 export const data_2020_01 = {
 	members : [
-		{
+		{	
+			empno : "10001",
 			name : "강성우",				// 이름
 			position : "대리",			// 직책
-			totalExpense : "50000"		// 월 경비 합계
+			totalExpense : "36000"		// 월 경비 합계
 		},
 		{
+			empno : "10003",
 			name : "김준선",				// 이름
 			position : "사원",			// 직책
-			totalExpense : "12000"		// 월 경비 합계
+			totalExpense : "20000"		// 월 경비 합계
 		},
 		{
+			empno : "10002",
 			name : "유기환",				// 이름
 			position : "사원",			// 직책
-			totalExpense : "12000"		// 월 경비 합계
+			totalExpense : "8000"		// 월 경비 합계
 		},
 	],
-	totalExpense : "74000",
+	expenseInfo : [
+		{
+			empno : "10001",			// 사번
+			no : "11",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200122",		// 결제일
+		},
+		{
+			empno : "10001",			// 사번
+			no : "10",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200120",		// 결제일
+		},
+		{
+			empno : "10001",			// 사번
+			no : "7",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20200122",		// 결제일
+		},
+		{
+			empno : "10002",			// 사번
+			no : "4",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20200111",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "3",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20200112",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "2",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20200105",		// 결제일
+		},
+	],
+	totalExpense : "102000",
 }
 
 export const data_2019_12 = {
 	members : [
 		{
+			empno : "10001",
 			name : "강성우",				// 이름
 			position : "대리",			// 직책
 			totalExpense : "10000"		// 월 경비 합계
 		},
 		{
+			empno : "10000",
 			name : "송원회",				// 이름
 			position : "대리",			// 직책
-			totalExpense : "40000"		// 월 경비 합계
+			totalExpense : "46000"		// 월 경비 합계
 		},
 		{
+			empno : "10003",
 			name : "김준선",				// 이름
 			position : "사원",			// 직책
-			totalExpense : "12000"		// 월 경비 합계
+			totalExpense : "16000"		// 월 경비 합계
+		},
+	],
+	expenseInfo : [
+		{
+			empno : "10001",			// 사번
+			no : "11",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191222",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "10",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191221",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "9",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191220",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "7",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191218",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "6",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191217",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "5",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20191212",		// 결제일
+		},
+		{
+			empno : "10003",			// 사번
+			no : "4",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "8000",	// 금액
+			payDate : "20191205",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "3",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20191204",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "2",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20191202",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "1",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시비",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20191201",		// 결제일
 		},
 	],
 	totalExpense : "62000",
@@ -44,11 +292,22 @@ export const data_2019_12 = {
 export const data_2019_11 = {
 	members : [
 		{
+			empno : "10002",
 			name : "유기환",				// 이름
 			position : "사원",			// 직책
 			totalExpense : "12000"		// 월 경비 합계
 		},
 
+	],
+	expenseInfo : [
+		{
+			empno : "10002",			// 사번
+			no : "11",					// 경비번호
+			expType : "도서",			// 경비유형
+			contents : "도서구매(교보문고) - React란 무엇인가?",			// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191120",		// 결제일
+		},
 	],
 	totalExpense : "12000",
 }
@@ -56,9 +315,20 @@ export const data_2019_11 = {
 export const data_2019_10 = {
 	members : [
 		{
+			empno : "10001",
 			name : "강성우",				// 이름
 			position : "대리",			// 직책
 			totalExpense : "12000"		// 월 경비 합계
+		},
+	],
+	expenseInfo : [
+		{
+			empno : "10001",			// 사번
+			no : "101",					// 경비번호
+			expType : "비품구매",			// 경비유형
+			contents : "종이컵 외 10건",		// 이름
+			ExpenseAmount : "12000",	// 금액
+			payDate : "20191001",		// 결제일
 		},
 	],
 	totalExpense : "12000",
@@ -67,14 +337,34 @@ export const data_2019_10 = {
 export const data_2019_09 = {
 	members : [
 		{
+			empno : "10003",
 			name : "김준선",				// 이름
 			position : "사원",			// 직책
 			totalExpense : "10000"		// 월 경비 합계
 		},
 		{
+			empno : "10002",
 			name : "유기환",				// 이름
 			position : "사원",			// 직책
 			totalExpense : "10000"		// 월 경비 합계
+		},
+	],
+	expenseInfo : [
+		{
+			empno : "10003",			// 사번
+			no : "103",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20190901",		// 결제일
+		},
+		{
+			empno : "10002",			// 사번
+			no : "104",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20190901",		// 결제일
 		},
 	],
 	totalExpense : "20000",
@@ -83,25 +373,48 @@ export const data_2019_09 = {
 export const data_2019_08 = {
 	members : [
 		{
+			empno : "10001",
 			name : "강성우",				// 이름
 			position : "대리",			// 직책
 			totalExpense : "10000"		// 월 경비 합계
 		},
 		{
+			empno : "10000",
 			name : "송원회",				// 이름
 			position : "대리",			// 직책
 			totalExpense : "9000"		// 월 경비 합계
 		},
 	],
+	expenseInfo : [
+		{
+			empno : "10001",			// 사번
+			no : "200",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시",			// 이름
+			ExpenseAmount : "10000",	// 금액
+			payDate : "20190801",		// 결제일
+		},
+		{
+			empno : "10000",			// 사번
+			no : "201",					// 경비번호
+			expType : "야간경비",			// 경비유형
+			contents : "택시",			// 이름
+			ExpenseAmount : "9000",	// 금액
+			payDate : "20190801",		// 결제일
+		},
+	],
 	totalExpense : "19000",
+	
 }
 
 export const data_2019_07 = {
 	members : [],
+	expenseInfo : [],
 	totalExpense : "0",
 }
 
 export const nondata = {
 	members : [],
-	totalExpense : "",
+	expenseInfo : [],
+	totalExpense : "0",
 }
