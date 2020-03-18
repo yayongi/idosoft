2061ba1e31772118697cb250e5b401d88ca58d4d
KangSeongWoo <34757991+KangSeongWoo@users.noreply.github.com>
2020-03-18 22:10:47 +0900

    Update data.js

--- react_src/intranet/views/Dashboard/components/Notice/data.js
+++ react_src/intranet/views/Dashboard/components/Notice/data.js
@@ -1,6 +1,6 @@
 export const data = [
-	{id:"1",title:"공지사항1 입니다.",date:"20190101",writer:"한화영"},
-	{id:"2",title:"공지사항2 입니다.",date:"20200101",writer:"최문걸"},
-	{id:"3",title:"공지사항3 입니다.",date:"20200301",writer:"한화영"},
-	{id:"4",title:"공지사항4 입니다.",date:"20190201",writer:"임정환"},
+	{id:"1",title:"공지사항1 입니다.",date:"20190101",writer:"한화영",content:"하하하하하하하하하핳"},
+	{id:"2",title:"공지사항2 입니다.",date:"20200101",writer:"최문걸",content:"하하하하하하하하하핳"},
+	{id:"3",title:"공지사항3 입니다.",date:"20200301",writer:"한화영",content:"하하하하하하하하하핳"},
+	{id:"4",title:"공지사항4 입니다.",date:"20190201",writer:"임정환",content:"하하하하하하하하하핳"},
 ]
