ec1b58310794c84293447b864f5df29a88b379ed
유경이 <@DESKTOP-FGUB9AH>
2020-03-18 21:49:41 +0900

    DropZone File Upload 적용

--- react_src/intranet/views/Expense/AnnualList/View/View.js
+++ react_src/intranet/views/Expense/AnnualList/View/View.js
@@ -23,13 +23,15 @@ import CurrencyTextField from '@unicef/material-ui-currency-textfield';
 import DateFnsUtils from '@date-io/date-fns';
 import ko from "date-fns/locale/ko";
 import Moment from "moment";
+
+import {useDropzone} from 'react-dropzone'
+
 import {
   MuiPickersUtilsProvider,
   DatePicker,
 } from '@material-ui/pickers';
 
 import { AnnualStorage, expenseTypes } from 'views/Expense/AnnualList/data';
-import moment from 'moment';
 
 
 const useStyles = makeStyles(theme => ({
@@ -65,6 +67,66 @@ const useStyles = makeStyles(theme => ({
 	},
 	
 }));
+
+const baseStyle = {
+	flex: 1,
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	padding: '20px',
+	borderWidth: 2,
+	borderRadius: 2,
+	borderColor: '#eeeeee',
+	borderStyle: 'dashed',
+	backgroundColor: '#fafafa',
+	color: '#bdbdbd',
+	outline: 'none',
+	transition: 'border .24s ease-in-out'
+};
+
+const activeStyle = {
+	borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+	borderColor: '#00e676'
+};
+
+const rejectStyle = {
+	borderColor: '#ff1744'
+};
+
+
+const thumbsContainer = {
+	display: 'flex',
+	flexDirection: 'row',
+	flexWrap: 'wrap',
+	marginTop: 16
+};
+
+const thumb = {
+	display: 'inline-flex',
+	borderRadius: 2,
+	border: '1px solid #eaeaea',
+	marginBottom: 8,
+	marginRight: 8,
+	width: 100,
+	height: 100,
+	padding: 4,
+	boxSizing: 'border-box'
+};
+
+const thumbInner = {
+	display: 'flex',
+	minWidth: 0,
+	overflow: 'hidden'
+};
+
+const img = {
+	display: 'block',
+	width: 'auto',
+	height: '100%'
+};
 
 
 function getStepInfo(row) {
@@ -113,6 +175,50 @@ const emptyData = {
 */
 export default function  View(props) {
 	console.log("call View Area");
+	const [files, setFiles] = React.useState([]);
+	const {
+		getRootProps,
+		getInputProps,
+		isDragActive,
+		isDragAccept,
+		isDragReject
+	} = useDropzone({
+		accept: 'image/*',
+		onDrop: acceptedFiles => {
+			console.log("file : " + JSON.stringify(acceptedFiles));
+			setFiles(acceptedFiles.map(file => Object.assign(file, {
+				preview: URL.createObjectURL(file)
+			})));
+		}
+	});
+	
+	const thumbs = files.map(file => (		
+		<div style={thumb} key={file.name}>
+			<div style={thumbInner}>
+				<img
+					src={file.preview}
+					style={img}
+				/>
+			</div>
+		</div>
+	));
+	
+	const style = React.useMemo(() => ({
+			...baseStyle,
+			...(isDragActive ? activeStyle : {}),
+			...(isDragAccept ? acceptStyle : {}),
+			...(isDragReject ? rejectStyle : {})
+		}), [
+			isDragActive,
+			isDragReject
+	]);
+
+	React.useEffect(() => () => {
+		// Make sure to revoke the data uris to avoid memory leaks
+		files.forEach(file => URL.revokeObjectURL(file.preview));
+	}, [files]);
+
+
 	const {routeProps, screenType } = props;
 	const {history, location, match} = routeProps;
 
@@ -384,7 +490,17 @@ export default function  View(props) {
 						</TableRow>
 						<TableRow>
 							<TableCell align="left" component="th" scope="row">첨부파일</TableCell>
-							<TableCell align="left"></TableCell>
+							<TableCell align="left">
+								<section className="container">
+									<div {...getRootProps({style})}>
+										<input {...getInputProps()} />
+										<p>여기에 이미지 파일을 끌어다 놓거나 클릭하세요.</p>
+									</div>
+									<aside style={thumbsContainer}>
+										{thumbs}
+									</aside>
+								</section>
+							</TableCell>
 						</TableRow>
 					</TableBody>
 				</Table>
